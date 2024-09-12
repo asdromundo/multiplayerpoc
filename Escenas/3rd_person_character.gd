ff6fbe718f12e1dde3522293ebb7356ffeb787b6
Antonio S. Dromundo <asdromundo@asdrome.com>
2024-09-12 11:01:59 -0800

    Functional 3rd Person Character

--- Escenas/3rd_person_character.gd
+++ Escenas/3rd_person_character.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
-const SPEED = 5
-const JUMP_VELOCITY = 5.5
-@onready var camera_3d = $Camera3D
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@onready var camera_mount = $CameraMount
 @onready var character = $GDbotSkin
+@export var camera_sensibility : float = 1.0
+var camera_speed = camera_sensibility * .005
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -11,9 +13,9 @@ func _ready():
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * .005)
-		camera_3d.rotate_x(-event.relative.y * .005)
-		camera_3d.rotation.x = clamp(camera_3d.rotation.x, -PI/4, PI/4)
+		rotate_y(-event.relative.x * camera_speed)
+		camera_mount.rotate_x(-event.relative.y * camera_speed)
+		camera_mount.rotation.x = clamp(camera_mount.rotation.x, -PI/8, PI/4)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -27,30 +29,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+
 	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
-
-	# Only rotate if there's movement
-	if direction.length() > 0:
-		# Rotate the character to face the direction of movement
-		look_at(global_transform.origin + direction * delta, Vector3.UP)
-		## Suavizar la rotación del personaje en la dirección del movimiento.
-		#var target_rotation = direction.angle_to(Vector3.FORWARD)
-		#rotation.y = lerp_angle(rotation.y, target_rotation, 2.0 * delta)
-
-		# Apply movement
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		character.look_at(position - direction)
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-
+		
 		if is_on_floor():
 			character.walk()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
 	if velocity == Vector3.ZERO:
 		character.idle()
-	
+
 	move_and_slide()
